90477c5c0f3b566d629400963504f79bccb9cdae
makobhakub <96353713+makobhakub@users.noreply.github.com>
2021-12-19 14:03:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+#Guess game
 import random as r
 
-guesses_taken = 0
+
 
 print('Hello! What is your name?')
 my_name = input()
-print('')
+
+number = r.randint(1,20)
+print(f'Well, {my_name} I am thinking of a number between 1 and 20.')
+    
+for guesses_taken in range(6):
+    print('take a guess')
+    guess = input()
+    guess = int(guess)
+    
+    if guess < number: 
+        print('that is less than my number')
+    elif guess > number
+        print('that is bigger than my numer')
+    else:
+        break 
+    
+if guess == number:
+    print ('good job, you win') 
