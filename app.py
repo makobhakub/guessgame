e9d6624892823519c1a01f5797312c697a1a1822
makobhakub <96353713+makobhakub@users.noreply.github.com>
2021-12-19 14:06:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 #Guess game
 import random as r
 
-
-
 print('Hello! What is your name?')
 my_name = input()
 
@@ -16,10 +14,12 @@ for guesses_taken in range(6):
     
     if guess < number: 
         print('that is less than my number')
-    elif guess > number
+    elif guess > number:
         print('that is bigger than my numer')
     else:
         break 
     
 if guess == number:
+    guesses_taken = guesses_taken + 1
+    print(f'good job, {my_name}!' you guessed my number in {guesses_taken} guesses!
     print ('good job, you win') 
