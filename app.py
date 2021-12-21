2459b1f3c012b5a70a6c8331eedf88cf99220dbe
makobhakub <96353713+makobhakub@users.noreply.github.com>
2021-12-21 16:02:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,34 @@ import random as edgar
 
 print('Hello! What is your name?')
 my_name = input()
-print('and how lucky do you think you are, how many guesses do you want?')
+print(f'{my_name} how lucky do you think you are? How many guesses do you want?')
 number_of_guesses = input()
 number_of_guesses = int(number_of_guesses)
-number = edgar.randint(1,20)
-print(f'Well, {my_name} I am thinking of a number between 1 and 20.')
+number_from = input('pick your smallest number for the range, what would you like it to be? ')
+number_from = int(number_from)
+number_to = input('pick your largest number for the range, what would you like it to be? ')
+number_to = int(number_to)
+number = edgar.randint(number_from, number_to)
+print(f'Well, {my_name} I am thinking of a number between {number_from} and {number_to}.')
     
 for guesses_taken in range(number_of_guesses):
-    print('take a guess')
+    
+    if guesses_taken > 0:
+        another_word = 'another'
+    else: 
+        another_word = 'a'
+    
+    print(f'take {another_word} guess')   
+    
     guess = input()
     guess = int(guess)
     
-    if guess < number: 
+    if guess < number_from or guess > number_to: 
+        print('that is out of range, FOOL!')
+    elif guess < number: 
         print('that is less than my number')
     elif guess > number:
-        print('that is bigger than my numer')
+        print('that is bigger than my number')
     else:
         break 
     
@@ -27,4 +40,3 @@ if guess == number:
 
 if guess != number:
     print(f'no, the number I was thinking of was {number}')
-
